5307430bedeb1ff5c497da769450e5dd281da29e
flaviorv <frezendevaroto@gmail.com>
2025-05-23 10:44:05 -0300

    The train and validation were splitted and the species were binarized to distinct Adelie from others species

--- features_target.py
+++ features_target.py
@@ -1,8 +1,13 @@
-import pandas as pd
+from sklearn.model_selection import train_test_split
 from palmerpenguins import load_penguins
 
 penguins = load_penguins()
 penguins = penguins.dropna()
 print(penguins.columns)
 features = ['island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex', 'year']
-target = 'species'
+penguins['is_adelie'] = penguins['species'].map(lambda s: 1 if s == "Adelie" else 0)
+target = penguins['is_adelie']
+
+x_train, x_val, y_train, y_val = train_test_split(penguins[features], target, test_size=0.2, random_state=20, stratify=target)
+
+print(len(x_train), len(x_val))
